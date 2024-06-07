dfed2a7fa680e6f4cda1ffa8c56040c05f44468d
proffatai <proffatai@gmail.com>
2024-06-07 23:08:32 +0100

    How to start an app using desired capabilities

--- src/baseClass/baseClass.java
+++ src/baseClass/baseClass.java
@@ -19,13 +19,23 @@ public class baseClass {
 	
 	public void AppiumTest() throws MalformedURLException, InterruptedException
 	{ 
-		UiAutomator2Options options = new UiAutomator2Options();
-
+//	UiAutomator2Options options = new UiAutomator2Options();
+//		options.setApp("//Users//proffatai//Documents//Appium projects//Boomplay_Appium_Automation//src//resources//boomplay.apk");
+//		options.setDeviceName("Pixel4"); 
 		
-		options.setApp("//Users//proffatai//Documents//Appium projects//Boomplay_Appium_Automation//src//resources//boomplay.apk");
-		options.setDeviceName("Pixel4"); 
+		 //Alternative way of starting the app by setting all the app capabilites
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "Pixel4");
+        capabilities.setCapability("appPackage", "com.afmobi.boomplayer");
+        capabilities.setCapability("appActivity", "com.tecno.boomplayer.guide.ControllerActivity");
+        capabilities.setCapability("automationName", "UiAutomator2");
+        capabilities.setCapability("noReset", true);
+        capabilities.setCapability("appWaitDuration", 10000); // 30 seconds
+		capabilities.setCapability("app", "//Users//proffatai//Documents//Appium projects//Boomplay_Appium_Automation//src//resources//boomplay.apk");
 		
-		driver = new AndroidDriver(new URL("http://127.0.0.1:4723"),options);
+	
+		driver = new AndroidDriver(new URL("http://127.0.0.1:4723"),capabilities);
 		
 		driver.findElement(AppiumBy.id("com.afmobi.boomplayer:id/bt_ok")).click(); 
 		Thread.sleep(4000); 
